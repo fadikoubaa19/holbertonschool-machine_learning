cf97fc620dbe4098506366d1d39daa8b8bf494bc
fadikoubaa19 <fadi.koubaa@gmail.com>
2021-05-25 14:53:59 +0100

    exp

---
+++ math/0x03-probability/exponential.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""function exp"""
+
+
+class Exponential:
+    """distrubute exp
+    """
+    e = 2.7182818285
+
+    def __init__(self, data=None, lambtha=1.):
+        """Initialisation"""
+        if data is None:
+            if lambtha <= 0:
+                raise ValueError('lambtha must be a positive value')
+            else:
+                self.lambtha = float(lambtha)
+        else:
+            if not isinstance(data, list):
+                raise TypeError('data must be a list')
+            if len(data) < 2:
+                raise ValueError('data must contain multiple values')
+            self.lambtha = 1 / (sum(data) / len(data))
