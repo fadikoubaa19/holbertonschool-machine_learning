70332af9cb2fd1b6003a19492f97af63e93152c9
fadikoubaa19 <fadi.koubaa@gmail.com>
2021-05-25 15:40:26 +0100

    new class normal

---
+++ math/0x03-probability/normal.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""Normal module"""
+
+
+class Normal:
+    """new class normal"""
+    e = 2.7182818285
+    pi = 3.1415926536
+
+    def __init__(self, data=None, mean=0., stddev=1.):
+        """function init"""
+        if data is None:
+            if stddev <= 0:
+                raise ValueError('stddev must be a positive value')
+            else:
+                self.mean = float(mean)
+                self.stddev = float(stddev)
+        else:
+            if not isinstance(data, list):
+                raise TypeError('data must be a list')
+            if len(data) < 2:
+                raise ValueError('data must contain multiple values')
+            self.mean = sum(data) / len(data)
+            s = 0
+            for item in data:
+                s += (item - self.mean)**2
+            self.stddev = (s / len(data))**.5
