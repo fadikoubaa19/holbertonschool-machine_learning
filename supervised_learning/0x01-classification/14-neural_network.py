c2b2bd74ab05c2fa28f1e6c0ae50790d8713adbf
fadikoubaa19 <2062@holbertonschool.com>
2021-07-04 23:38:09 +0100

    function to train net

--- supervised_learning/0x01-classification/14-neural_network.py
+++ supervised_learning/0x01-classification/14-neural_network.py
@@ -91,7 +91,7 @@ class NeuralNetwork:
             raise TypeError("alpha must be a float")
         if alpha <= 0:
             raise ValueError("alpha must be positive")
-        for * in range(iterations):
+        for i in range(iterations):
             self.forward_prop(X)
             self.gradient_descent(X, Y, self.__A1, self.__A2, alpha)
         return self.evaluate(X, Y)
