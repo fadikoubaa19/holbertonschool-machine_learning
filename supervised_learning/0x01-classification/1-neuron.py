43cb63f1c0bcee46e5f49912a06fb2d86fbef863
fadikoubaa19 <2062@holbertonschool.com>
2021-07-04 21:19:38 +0100

    fixed

--- supervised_learning/0x01-classification/1-neuron.py
+++ supervised_learning/0x01-classification/1-neuron.py
@@ -1,25 +1,23 @@
 #!/usr/bin/env python3
 """
-defines a single neuron performing binary classification:
+function 1-neruon
 """
-
 
 import numpy as np
 
 
 class Neuron:
-    """Class named neuro"""
+    """ class neuro"""
 
     def __init__(self, nx):
         """function init"""
-        if not type(nx) is int:
-            raise TypeError('nx must be an integer')
-        if nx < 1:
-            raise ValueError('nx must be a positive integer')
-
-        self.W = np.random.randn(1, nx)
-        self.b = 0
-        self.A = 0
+        if type(nx) != int:
+            raise TypeError("nx must be an integer")
+        elif nx < 1:
+            raise ValueError("nx must be a positive integer")
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
 
     @property
     def W(self):
