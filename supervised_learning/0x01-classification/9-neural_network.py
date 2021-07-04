9f6bfc484871d6d1c23d688ac2b1550fb88ed3b8
fadikoubaa19 <2062@holbertonschool.com>
2021-07-04 22:23:59 +0100

    fixed

--- supervised_learning/0x01-classification/9-neural_network.py
+++ supervised_learning/0x01-classification/9-neural_network.py
@@ -31,20 +31,20 @@ class NeuralNetwork:
         return self.__W1
 
     @property
-    def b1(self):
-        return self.__b1
-
-    @property
-    def A1(self):
-        return self.__A1
-
-    @property
     def W2(self):
         return self.__W2
 
     @property
+    def b1(self):
+        return self.__b1
+
+    @property
     def b2(self):
         return self.__b2
+
+    @property
+    def A1(self):
+        return self.__A1
 
     @property
     def A2(self):
