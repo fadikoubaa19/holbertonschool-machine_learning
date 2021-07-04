470b622c421d954a076d7490804b38850a77e2ee
fadikoubaa19 <2062@holbertonschool.com>
2021-07-04 23:46:23 +0100

    get fixed please

--- supervised_learning/0x01-classification/14-neural_network.py
+++ supervised_learning/0x01-classification/14-neural_network.py
@@ -83,12 +83,12 @@ class NeuralNetwork:
 
     def train(self, X, Y, iterations=5000, alpha=0.05):
         """function to train network"""
-        if type(iterations) != int:
+        if type(iterations) is not int:
             raise TypeError("iterations must be an integer")
         if iterations < 0:
             raise ValueError("iterations must be a positive integer")
 
-        if type(alpha) != float:
+        if type(alpha) is not float:
             raise TypeError("alpha must be a float")
         if alpha < 0:
             raise ValueError("alpha must be positive")
