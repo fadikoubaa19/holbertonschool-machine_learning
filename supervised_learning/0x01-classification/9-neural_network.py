c6a9e7f73e780b56082b69fad583b54e48ee594c
fadikoubaa19 <2062@holbertonschool.com>
2021-07-04 22:21:11 +0100

    add 6 prop

---
+++ supervised_learning/0x01-classification/9-neural_network.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+"""
+function neural network
+"""
+
+import numpy as np
+
+
+class NeuralNetwork:
+    """ new Class added NeuralNetwork"""
+    def __init__(self, nx, nodes):
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if type(nodes) is not int:
+            raise TypeError("nodes must be an integer")
+        if nodes < 1:
+            raise ValueError("nodes must be a positive integer")
+        self.nx = nx
+        self.nodes = nodes
+        self.W1 = np.random.randn(nodes, nx)
+        self.b1 = np.zeros((nodes, 1))
+        self.A1 = 0
+        self.W2 = np.random.randn(1, nodes)
+        self.b2 = 0
+        self.A2 = 0
+
+    @property
+    def W1(self):
+        """W1 prop"""
+        return self.__W1
+
+    @property
+    def b1(self):
+        """ADD b1 prop"""
+        return self.__b1
+
+    @property
+    def A1(self):
+        """ADD A1 prop"""
+        return self.__A1
+
+    @property
+    def W2(self):
+        """ADD W2 prop"""
+        return self.__W2
+
+    @property
+    def b2(self):
+        """ADD b2 prop"""
+        return self.__b2
+
+    @property
+    def A2(self):
+        """ADD A2 prop"""
+        return self.__A2
