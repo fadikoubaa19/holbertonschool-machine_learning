6ea980ac86ff73de6a3e02c240635963144defd8
fadikoubaa19 <2062@holobertonschool.com>
2021-07-05 23:41:26 +0100

    forward

--- supervised_learning/0x02-tensorflow/2-forward_prop.py
+++ supervised_learning/0x02-tensorflow/2-forward_prop.py
@@ -9,7 +9,8 @@ create_layer = __import__('1-create_layer').create_layer
 
 def forward_prop(x, layer_sizes=[], activations=[]):
     """ create prop layer"""
-    pred = create_layer(x, layer_sizes[0], activations[0])
-    for i in range(1, len(layer_sizes)):
-        pred = create_layer(pred, layer_sizes[i], activations[i])
+    pred = x
+    for i in range(len(layer_sizes)):
+        pred = create_layer(pred, layer_sizes[i],
+                            activation=activations[i])
     return pred
