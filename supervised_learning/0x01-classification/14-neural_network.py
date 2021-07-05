5bcd20cde51b36ef1558d269c4192d3b09be0e64
fadikoubaa19 <2062@holbertonschool.com>
2021-07-05 19:02:17 +0100

    function train added and get fixed

--- supervised_learning/0x01-classification/14-neural_network.py
+++ supervised_learning/0x01-classification/14-neural_network.py
@@ -82,17 +82,16 @@ class NeuralNetwork:
         self.__b1 += - alpha * np.sum(dz1, axis=1, keepdims=True) / m
 
     def train(self, X, Y, iterations=5000, alpha=0.05):
-        """function to train network"""
-        if type(iterations) is not int:
-            raise TypeError("iterations must be an integer")
+        """function train"""
+        if not isinstance(iterations, int):
+            raise TypeError('iterations must be an integer')
         if iterations < 0:
-            raise ValueError("iterations must be a positive integer")
-
-        if type(alpha) is not float:
-            raise TypeError("alpha must be a float")
+            raise ValueError('iterations must be a positive integer')
+        if not isinstance(alpha, float):
+            raise TypeError('alpha must be a float')
         if alpha < 0:
-            raise ValueError("alpha must be positive")
-        for i in range(iterations):
-            self.__A1, self.__A2 = self.forward_prop(X)
-            self.gradient_descent(X, Y, self.__A1, self.__A2, alpha)
+            raise ValueError('alpha must be positive')
+        for _ in range(iterations):
+            self.forward_prop(X)
+            self.gradient_descent(X, Y, self.A1, self.A2, alpha)
         return self.evaluate(X, Y)
