8e37299250da72683ee87b277aadb40b23fac4d1
fadikoubaa19 <2062@holobertonschool.com>
2021-07-06 00:01:43 +0100

    evaluate

--- supervised_learning/0x02-tensorflow/7-evaluate.py
+++ supervised_learning/0x02-tensorflow/7-evaluate.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python3
 """
-7-evaluate
+7-evaluate function
 """
 
 import tensorflow as tf
 
 
 def evaluate(X, Y, save_path):
-    """evaluation of neural network"""
+    """
+    evaluate the loss
+    """
     with tf.Session() as sess:
-        salid = tf.train.import_meta_graph("{}.meta".format(save_path))
-        salid.restore(sess, save_path)
+        saver = tf.train.import_meta_graph("{}.meta".format(save_path))
+        saver.restore(sess, save_path)
 
         x = tf.get_collection("x")[0]
         y = tf.get_collection("y")[0]
@@ -21,4 +23,4 @@ def evaluate(X, Y, save_path):
         eval_y_pred = sess.run(y_pred, feed_dict={x: X, y: Y})
         eval_accuracy = sess.run(accuracy, feed_dict={x: X, y: Y})
         eval_loss = sess.run(loss, feed_dict={x: X, y: Y})
-        return eval_y_pred, eval_accuracy, eval_l
+        return eval_y_pred, eval_accuracy, eval_loss
