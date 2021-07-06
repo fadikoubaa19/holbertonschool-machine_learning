537d5fe52452d5c77d8a2d58771082d3b515e18c
fadikoubaa19 <2062@holbertonschool.com>
2021-07-06 22:28:10 +0100

    updates the learning rate using inverse time decay

--- supervised_learning/0x03-optimization/11-learning_rate_decay.py
+++ supervised_learning/0x03-optimization/11-learning_rate_decay.py
@@ -7,5 +7,5 @@ def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
     """ updates the learning rate using inverse time decay"""
 
     rt = decay_rate
-    ur = alpha / (1 + dr * int(global_step / decay_step))
+    ur = alpha / (1 + rt * int(global_step / decay_step))
     return ur
