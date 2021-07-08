c42b1322587d900514d15b885e69067fb704b23d
fadikoubaa19 <2062@holbertonschool.com>
2021-07-08 21:30:01 +0100

    cal the prec

--- supervised_learning/0x04-error_analysis/2-precision.py
+++ supervised_learning/0x04-error_analysis/2-precision.py
@@ -4,10 +4,10 @@ import numpy as np
 
 
 def precision(confusion):
-    """Calculates the precision for each class in a confusion matrix
-    """
+    """Calc the precision"""
+
     CL = confusion.sum(axis=0) - np.diag(confusion)
     CA = confusion.sum(axis=1) - np.diag(confusion)
     CM = np.diag(confusion)
-    TN = confusion.sum() - (CL + CA + CM)
+    CF = confusion.sum() - (CL + CA + CM)
     return CM / (CM + CL)
