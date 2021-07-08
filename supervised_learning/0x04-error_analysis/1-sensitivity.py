c0fb560437da12cfabf13d12fd120b3049ffff3d
fadikoubaa19 <2062@holbertonschool.com>
2021-07-08 21:18:29 +0100

    calc the sens

--- supervised_learning/0x04-error_analysis/1-sensitivity.py
+++ supervised_learning/0x04-error_analysis/1-sensitivity.py
@@ -4,9 +4,10 @@ import numpy as np
 
 
 def sensitivity(confusion):
-    """Calculates the sensitivity for each class in a confusion matrix"""
-    a = confusion.sum(axis=0) - np.diag(confusion)
-    b = confusion.sum(axis=1) - np.diag(confusion)
-    c = np.diag(confusion)
-    sm = confusion.sum() - (a + b + c)
-    return c / (c + b)
+    """function that Calculates the sens"""
+
+    a = np.sum(confusion, axis=1)
+    a_positive = np.diagonal(confusion)
+    result = a_positive / a
+
+    return np.array(result)
