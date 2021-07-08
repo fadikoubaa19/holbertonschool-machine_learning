639478aca4e2915d35cac485ddad4fea6f13e54a
fadikoubaa19 <2062@holbertonschool.com>
2021-07-08 21:42:18 +0100

    sensitivity calc

--- supervised_learning/0x04-error_analysis/1-sensitivity.py
+++ supervised_learning/0x04-error_analysis/1-sensitivity.py
@@ -6,8 +6,8 @@ import numpy as np
 def sensitivity(confusion):
     """function that Calculates the sens"""
 
-    a = np.sum(confusion, axis=1)
-    a_positive = np.diagonal(confusion)
-    result = a_positive / a
+    CL = np.diag(confusion)
+    CA = np.sum(confusion, axis=1) - CL
+    sensitivity = CL / (CL + CA)
 
-    return np.array(result)
+    return sensitivity
