0d666563aa53febd08b828902873244548b87046
fadikoubaa19 <2062@holbertonschool.com>
2021-07-11 19:18:09 +0100

    fixed again

--- supervised_learning/0x06-keras/0-sequential.py
+++ supervised_learning/0x06-keras/0-sequential.py
@@ -7,17 +7,14 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
 
     """task0"""
     model = keras.Sequential()
-    regu = keras.regularizers.l2(lambtha)
-
+    regularizer = keras.regularizers.l2(lambtha)
     model.add(keras.layers.Dense(units=layers[0],
                                  activation=activations[0],
-                                 kernel_regularizer=regu,
+                                 kernel_regularizer=regularizer,
                                  input_shape=(nx,)))
-
     for i in range(1, len(layers)):
         model.add(keras.layers.Dropout(1 - keep_prob))
         model.add(keras.layers.Dense(units=layers[i],
                                      activation=activations[i],
-                                     kernel_regularizer=regu))
-
+                                     kernel_regularizer=regularizer))
     return model
