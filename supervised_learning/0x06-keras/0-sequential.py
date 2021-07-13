a7e2591db658926aa4364e1a985dee640385f6e7
fadikoubaa19 <2062@holbertonschool.com>
2021-07-13 20:44:05 +0100

    0-seq

--- supervised_learning/0x06-keras/0-sequential.py
+++ supervised_learning/0x06-keras/0-sequential.py
@@ -4,17 +4,18 @@ import tensorflow.keras as K
 
 
 def build_model(nx, layers, activations, lambtha, keep_prob):
+    """function build_model"""
 
-    """task0"""
-    model = keras.Sequential()
-    regularizer = keras.regularizers.l2(lambtha)
-    model.add(keras.layers.Dense(units=layers[0],
-                                 activation=activations[0],
-                                 kernel_regularizer=regularizer,
-                                 input_shape=(nx,)))
-    for i in range(1, len(layers)):
-        model.add(keras.layers.Dropout(1 - keep_prob))
-        model.add(keras.layers.Dense(units=layers[i],
-                                     activation=activations[i],
-                                     kernel_regularizer=regularizer))
+    model = K.Sequential()
+    L2 = K.regularizers.l2(lambtha)
+    n = len(layers)
+    for x in range(n):
+        if x == 0:
+            model.add(K.layers.Dense(layers[x], activation=activations[x],
+                      kernel_regularizer=L2, input_shape=(nx,)))
+        else:
+            model.add(K.layers.Dense(layers[x], activation=activations[x],
+                      kernel_regularizer=L2))
+        if x < n - 1:
+            model.add(K.layers.Dropout(1 - keep_prob))
     return model
