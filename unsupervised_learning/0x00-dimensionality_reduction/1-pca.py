78c72c699ac6c2b72a12a3026fa0d6e90ecb188a
fadikoubaa19 <2062@holbertonschool.com>
2021-07-27 15:40:32 +0100

    file 2 prefom PCA of dataset

---
+++ unsupervised_learning/0x00-dimensionality_reduction/1-pca.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+1-pca.py: Prefom PCA  on a data set
+"""
+import numpy as np
+
+
+def pca(X, ndim):
+    """
+    X is the numpy of shape (n, d)
+    n the number of dataset
+    d is the number of dimention in each point
+    ndim is the new demensionality of the transformed X
+    Return  :  T, shape (n, ndim)
+    """
+    # X the numpy of n darry of shape
+
+    means = X - np.mean(X, axis=0)
+
+    # Using numpy (linear algebre)
+    P1, P2, P3 = np.linalg.svd(means)
+
+    W = vh.T
+    Wr = W[:, 0:ndim]
+
+    T = means @ Wr
+    return T
