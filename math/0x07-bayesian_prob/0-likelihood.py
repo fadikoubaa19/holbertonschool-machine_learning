c88fa5e310f773f2ba007b99008da95a5f86f922
fadikoubaa19 <2062@holbertonschool.com>
2021-07-29 20:43:16 +0100

    5 th try

--- math/0x07-bayesian_prob/0-likelihood.py
+++ math/0x07-bayesian_prob/0-likelihood.py
@@ -10,26 +10,19 @@ def likelihood(x, n, P):
     P: is 1  numpy.ndarry
     """
 
-    # if n is not int or equl to 0 raise value error inc a msg
     if not(type(n) is int) or n <= 0:
         raise ValueError("n must be a positive integer")
-
-    # if not x is not a int or equal to 0 raise value error with msg
     if not(type(x) is int) or x < 0:
-        raise ValueError(
-            "x must be an integer that is greater than or equal to 0")
-
-    # if x is greater than n raise value error inc a msg
+        msg = "x must be an integer that is greater than or equal to 0"
+        raise ValueError(msg)
     if x > n:
         raise ValueError("x cannot be greater than n")
-
-    # if P is not 1D or P isn"t in range raise a type error inc msg
     if type(P) != np.ndarray or len(P.shape) != 1:
         raise TypeError("P must be a 1D numpy.ndarray")
-    for idx in P:
-        if not (idx >= 0 and i <= 1):
-            raise ValueError("All values in {P} must be in the range [0, 1]")
-
+    for i in P:
+        if not (i >= 0 and i <= 1):
+            msg = "All values in P must be in the range [0, 1]"
+            raise ValueError(msg)
     # factorial of n
     fn = np.math.factorial(n)
 
