0eaace8729bf36f88eafe24e2e5e1d6d3bd8418f
fadikoubaa19 <2062@holbertonschool.com>
2021-07-29 20:36:08 +0100

    100 advanced task

---
+++ math/0x07-bayesian_prob/100-continuous.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""Calculates the posterior probability of a uniform distribution"""
+from scipy import special
+
+
+def posterior(x, n, p1, p2):
+    """
+    """
+    if not (type(n) is int) or n <= 0:
+        raise ValueError("n must be a positive integer")
+    if not (type(x) is int) or x < 0:
+        raise ValueError(
+            "x must be an integer that is greater than or equal to 0")
+
+    # if x is gretaer then n Value error with msg
+    if x > n:
+        raise ValueError("x cannot be greater than n")
+
+    # if p1 & p2 isn't a float or 0 or 1 value error a msg
+    if type(p1) is not float or not 0 <= p1 <= 1:
+        raise ValueError("p1 must be a float in the range [0, 1]")
+    if type(p2) is not float or not 0 <= p2 <= 1:
+        raise ValueError("p2 must be a float in the range [0, 1]")
+    if p2 <= p1:
+        raise ValueError("p2 must be greater than p1")
+
+    valuealpha = special.btdtr(x + 1, n - x + 1, p1)
+    valuedelta = special.btdtr(x + 1, n - x + 1, p2)
+
+    sum = valuealpha - valuedelta
+
+    return sum
