20f267fa5195dd056b2da45f8e5791350299aa97
fadikoubaa19 <2062@holbertonschool.com>
2021-10-05 14:43:54 +0100

    semantic search

---
+++ supervised_learning/0x13-qa_bot/3-semantic_search.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""Create function that performs semantic search """
+
+
+import numpy as np
+import os
+import tensorflow_hub as hub
+
+
+def semantic_search(corpus_path, sentence):
+    """ function semantic_search"""
+
+    document = [sentence]
+
+    for i in os.listdir(corpus_path):
+        if not i.endswith('.md'):
+            continue
+        with open(corpus_path + '/' + i, 'r', encoding='utf-8') as file:
+            document.append(file.read())
+    #Load the link.
+    link = hub.load(
+        'https://tfhub.dev/google/universal-sentence-encoder-large/5')
+
+    #embedding the link of doc
+    embeddings = link(document)
+
+    sl = np.inner(embeddings, embeddings)
+    ex = np.argmax(sl[0, 1:])
+
+    return document[ex + 1]
