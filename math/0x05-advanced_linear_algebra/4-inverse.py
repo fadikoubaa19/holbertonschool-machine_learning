2fe7755870aa603e2d0f7ced8bca667850f899d2
fadikoubaa19 <2062@holbertonschool.com>
2021-10-12 13:27:14 +0100

    function to inverse

---
+++ math/0x05-advanced_linear_algebra/4-inverse.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python3
+"""function that cal the matrix of matrix"""
+
+
+def determinant(matrix):
+    """
+    Function determinant:
+    param: ---> matrix
+    """
+    if type(matrix) is not list or len(matrix) == 0:
+        raise TypeError("matrix must be a list of lists")
+    if matrix == [[]]:
+        return 1
+    for a in range(len(matrix)):
+        if type(matrix[a]) is not list:
+            raise TypeError("matrix must be a list of lists")
+        if len(matrix) != len(matrix[a]):
+            raise ValueError("matrix must be a square matrix")
+    if len(matrix) == 1:
+        return matrix[0][0]
+
+    if len(matrix) == 2:
+        return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
+
+    x = 1
+    y = 0
+    for a in range(len(matrix)):
+        copy = []
+        for elements in matrix:
+            copy.append(elements.copy())
+
+        new_matrix = copy[1:]
+        for delete in new_matrix:
+            del delete[a]
+
+        y += matrix[0][a] * determinant(new_matrix) * x
+        x = x * -1
+
+    return y
+
+
+def cofactor(matrix):
+    """
+    Function cofactor
+    param: --> matrix
+    """
+    if type(matrix) is not list or len(matrix) == 0:
+        raise TypeError("matrix must be a list of lists")
+
+    if matrix == [[]]:
+        raise ValueError("matrix must be a non-empty square matrix")
+
+    for a in range(len(matrix)):
+        if type(matrix[a]) is not list:
+            raise TypeError("matrix must be a list of lists")
+        if len(matrix) != len(matrix[a]) or len(matrix[a]) == 0:
+            raise ValueError("matrix must be a non-empty square matrix")
+
+    if len(matrix) == 1:
+        return [[1]]
+
+    new_matrix = []
+    for a in range(len(matrix)):
+        aux = []
+        for b in range(len(matrix)):
+            copy = []
+            for elements in matrix:
+                copy.append(elements.copy())
+            del copy[a]
+            for delete in copy:
+                del delete[b]
+
+            if (a+b) % 2 != 0:
+                y = -1
+            else:
+                y = 1
+            aux.append(determinant(copy) * y)
+        new_matrix.append(aux)
+    return new_matrix
+
+
+def adjugate(matrix):
+    """
+    Function adjugate:
+    param: --> matrix
+    """
+    coffar = cofactor(matrix)
+    adjacente = []
+
+    for i in range(len(matrix)):
+        copy = []
+        for j in range(len(matrix)):
+            copy.append(coffar[j][i])
+        adjacente.append(copy)
+
+    return adjacente
+
+
+def inverse(matrix):
+    """
+    function inverse
+    """
+
+    coffar = adjugate(matrix)
+    adjacente = determinant(matrix)
+    if adjacente == 0:
+        return None
+    inv = []
+    for a in range(len(coffar)):
+        aux = []
+        for b in range(len(coffar)):
+            aux.append(coffar[a][b] / adjacente)
+        inv.append(aux)
+    return inv
