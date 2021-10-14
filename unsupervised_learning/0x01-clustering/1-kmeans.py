d8ae34f7ddf93c1d0a3417fd590e4ce24f3935b6
fadikoubaa19 <2062@holbertonschool.com>
2021-10-14 13:33:49 +0100

    kmeans

---
+++ unsupervised_learning/0x01-clustering/1-kmeans.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""1-Kmeans.py"""
+import numpy as np
+
+
+def kmeans(X, k, iterations=1000):
+    """
+    function kmeans: param:X,K,iterations:
+    """
+    if type(X) is not np.ndarray or len(X.shape) != 2:
+        return (None, None)
+    if type(k) is not int or k <= 0:
+        return (None, None)
+    if type(iterations) is not int or iterations <= 0:
+        return (None, None)
+
+    o, d = X.shape
+    min = np.min(X, axis=0)
+    max = np.max(X, axis=0)
+    vari = np.random.uniform(min, max, size=(k, d))
+
+    for i in range(iterations):
+        copy = vari.copy()
+        ins = np.linalg.norm((X - vari[:, np.newaxis]), axis=2)
+        init = ins.argmin(axis=0)
+        for j in range(k):
+            if (X[init == j].size == 0):
+                vari[j] = np.random.uniform(min, max, size=(1, d))
+            else:
+                vari[j] = (X[init == j].mean(axis=0))
+        ins = np.linalg.norm((X - vari[:, np.newaxis]), axis=2)
+        init = ins.argmin(axis=0)
+        if (copy == vari).all():
+            break
+
+    return vari, init
+
