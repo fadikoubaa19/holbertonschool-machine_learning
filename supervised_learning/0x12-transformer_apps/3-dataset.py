efa9673f56100a820e3dab02c47f8bac0c8ca251
fadikoubaa19 <2062@holbertonschool.com>
2021-10-21 12:38:58 +0100

    dataset.py

---
+++ supervised_learning/0x12-transformer_apps/3-dataset.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python3
+""" task 1 """
+import tensorflow.compat.v2 as tf
+import tensorflow_datasets as tfds
+
+
+class Dataset:
+    """loads and preps a dataset for machine translation"""
+    def __init__(self, batch_size, max_len):
+        """function init"""
+        def filter_max_length(x, y, max_length=max_len):
+            """
+            filter function
+            """
+            return tf.logical_and(tf.size(x) <= max_length,
+                                  tf.size(y) <= max_length)
+
+        examples, metadata = tfds.load('ted_hrlr_translate/pt_to_en',
+                                       with_info=True,
+                                       as_supervised=True)
+
+        self.data_train = examples['train']
+        self.data_valid = examples['validation']
+
+        PT, EN = self.tokenize_dataset(self.data_train)
+        self.tokenizer_pt, self.tokenizer_en = PT, EN
+
+        self.data_train = self.data_train.map(self.tf_encode)
+        self.data_train = self.data_train.filter(filter_max_length)
+        self.data_train = self.data_train.cache()
+
+        shu = metadata.splits['train'].num_examples
+        self.data_train = self.data_train.shuffle(shu)
+        pad_shape = ([None], [None])
+        self.data_train = self.data_train.padded_batch(batch_size,
+                                                       padded_shapes=pad_shape)
+        aux = tf.data.experimental.AUTOTUNE
+        self.data_train = self.data_train.prefetch(aux)
+
+        self.data_valid = self.data_valid.map(self.tf_encode)
+        self.data_valid = self.data_valid.filter(filter_max_length)
+        self.data_valid = self.data_valid.padded_batch(batch_size,
+                                                       padded_shapes=pad_shape)
+
+    def tokenize_dataset(self, data):
+        """
+        ---Function dataset---
+        param:
+        --> self, data
+        """
+
+        tokenizer_pt = tfds.features.text.SubwordTextEncoder.build_from_corpus(
+                (pt.numpy() for pt, en in data),
+                target_vocab_size=2**15)
+
+        tokenizer_en = tfds.features.text.SubwordTextEncoder.build_from_corpus(
+                (en.numpy() for pt, en in data),
+                target_vocab_size=2**15)
+
+        return tokenizer_pt, tokenizer_en
+
+    def encode(self, pt, en):
+        """
+        ---function encode---
+        param:
+        --> self, pt, en
+        """
+
+        pt_tokens = [self.tokenizer_pt.vocab_size] + self.tokenizer_pt.encode(
+            pt.numpy()) + [self.tokenizer_pt.vocab_size + 1]
+
+        en_tokens = [self.tokenizer_en.vocab_size] + self.tokenizer_en.encode(
+            en.numpy()) + [self.tokenizer_en.vocab_size + 1]
+
+        return pt_tokens, en_tokens
+
+    def tf_encode(self, pt, en):
+        """
+        function tf_encode:
+        param: self,pt ,en
+        """
+        result_pt, result_en = tf.py_function(self.encode,
+                                              [pt, en],
+                                              [tf.int64, tf.int64])
+        result_pt.set_shape([None])
+        result_en.set_shape([None])
+
+        return result_pt, result_en
