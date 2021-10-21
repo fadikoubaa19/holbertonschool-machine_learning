9a8c0dbd3732ccd553ae634807cc02e318ccd37a
fadikoubaa19 <2062@holbertonschool.com>
2021-10-21 12:40:58 +0100

    done

--- supervised_learning/0x12-transformer_apps/3-dataset.py
+++ supervised_learning/0x12-transformer_apps/3-dataset.py
@@ -4,43 +4,55 @@ import tensorflow.compat.v2 as tf
 import tensorflow_datasets as tfds
 
 
-class Dataset:
-    """loads and preps a dataset for machine translation"""
+class Dataset():
+    """ classe dataset """
+
     def __init__(self, batch_size, max_len):
-        """function init"""
-        def filter_max_length(x, y, max_length=max_len):
-            """
-            filter function
-            """
-            return tf.logical_and(tf.size(x) <= max_length,
-                                  tf.size(y) <= max_length)
+        """
+        function init:
+        pram:--> self, batch_size, max_len.
+        """
 
         examples, metadata = tfds.load('ted_hrlr_translate/pt_to_en',
                                        with_info=True,
                                        as_supervised=True)
 
+        self.metadata = metadata
+
         self.data_train = examples['train']
         self.data_valid = examples['validation']
 
-        PT, EN = self.tokenize_dataset(self.data_train)
-        self.tokenizer_pt, self.tokenizer_en = PT, EN
+        self.tokenizer_pt, self.tokenizer_en = self.tokenize_dataset(
+            self.data_train)
 
         self.data_train = self.data_train.map(self.tf_encode)
+        self.data_valid = self.data_valid.map(self.tf_encode)
+
+        def filter_max_length(x, y, max_length=max_len):
+            return tf.logical_and(tf.size(x) <= max_length,
+                                  tf.size(y) <= max_length)
+
         self.data_train = self.data_train.filter(filter_max_length)
         self.data_train = self.data_train.cache()
 
-        shu = metadata.splits['train'].num_examples
-        self.data_train = self.data_train.shuffle(shu)
-        pad_shape = ([None], [None])
-        self.data_train = self.data_train.padded_batch(batch_size,
-                                                       padded_shapes=pad_shape)
-        aux = tf.data.experimental.AUTOTUNE
-        self.data_train = self.data_train.prefetch(aux)
+        train_dataset_size = self.metadata.splits['train'].num_examples
 
-        self.data_valid = self.data_valid.map(self.tf_encode)
+        self.data_train = self.data_train.shuffle(
+            train_dataset_size)
+
+        padded_shapes = ([None], [None])
+        self.data_train = self.data_train.padded_batch(
+            batch_size, padded_shapes=padded_shapes)
+
+        self.data_train = self.data_train.prefetch(
+            tf.data.experimental.AUTOTUNE)
+
         self.data_valid = self.data_valid.filter(filter_max_length)
-        self.data_valid = self.data_valid.padded_batch(batch_size,
-                                                       padded_shapes=pad_shape)
+
+        padded_shapes = ([None], [None])
+        self.data_valid = \
+            self.data_valid.padded_batch(batch_size,
+                                         padded_shapes=padded_shapes)
 
     def tokenize_dataset(self, data):
         """
