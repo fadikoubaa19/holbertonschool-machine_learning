9270afe7e4d3a5449998983e0740a07c73f99faf
fadikoubaa19 <66315303+fadikoubaa19@users.noreply.github.com>
2021-10-25 14:03:19 +0100

    variance

--- unsupervised_learning/0x01-clustering/2-variance.py
+++ unsupervised_learning/0x01-clustering/2-variance.py
@@ -11,11 +11,17 @@ def variance(X, C):
         return None
     if type(C) is not np.ndarray or len(C.shape) != 2:
         return None
+    
+    # numpy.ndarray of shape (n, d):
     if X.shape[1] != C.shape[1]:
         return None
 
     init = np.square(X - C[:, np.newaxis]).sum(axis=2)
+    
+    # cal the minimum using np:
     distances = np.min(init, axis=0)
+
+    # cal the sum with np:
     result = np.sum(distances)
 
     return result
