5f43dd78df1ae27cbd4c17eb8e77e417c3b10f50
fadikoubaa19 <2062@holbertonschool.com>
2022-03-22 17:37:16 +0100

    some modif

--- reinforcement_learning/0x00-q_learning/1-q_init.py
+++ reinforcement_learning/0x00-q_learning/1-q_init.py
@@ -7,7 +7,7 @@ import numpy as np
 
 def q_init(env):
     """env --> FrozenLakeEnv // return q_table using numpy of zeros"""
-    s1 = env.action_space.n, a1 = env.observation_space.n
+    action = (env.observation_space.n, env.action_space.n)
 
     # np.zeros return array filled with zeros
-    return np.zeros((s1, a1))
+    return np.zeros((action))
