e7d9702b43faf7090afdc16fefed653f95df44d6
fadikoubaa19 <fadi.koubaa@gmail.com>
2022-09-07 16:06:37 +0100

    fixing

--- pipeline/0x01-apis/0-passengers.py
+++ pipeline/0x01-apis/0-passengers.py
@@ -19,7 +19,8 @@ def availableShips(passengerCount):
                 if int(ship['passengers']) >= passengerCount:
                     ships.append(ship['name'])
             except ValueError:
-                if ship['name'] == 'Executor' and ((passengerCount == 99999) or (passengerCount < 99999)):
+                if ship['name'] == 'Executor' and ((passengerCount == 99999)
+                   or (passengerCount < 99999)):
                     ships.append('Executor')
 
         url = data['next']
