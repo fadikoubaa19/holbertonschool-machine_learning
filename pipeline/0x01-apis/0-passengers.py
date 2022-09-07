de60b9bc878c50f3e1d35fceaa8297e707d89df2
fadikoubaa19 <fadi.koubaa@gmail.com>
2022-09-07 16:00:34 +0100

    please work

--- pipeline/0x01-apis/0-passengers.py
+++ pipeline/0x01-apis/0-passengers.py
@@ -1,5 +1,4 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
+#!/usr/bin/python3
 
 """hold number of passenger from star wars api"""
 
@@ -9,15 +8,20 @@ import requests
 def availableShips(passengerCount):
     """funct avaibleships"""
 
+    url = "https://swapi-api.hbtn.io/api/starships"
     ships = []
-    url = 'https://swapi-api.hbtn.io/api/starships'
+
     while url is not None:
-        info = requests.get(url).json()
-        for ship in info['results']:
-            passengers = ship['passengers'].replace(',', '')
-            if passengers == 'n/a' or passengers == 'unknown':
-                passengers = -1
-            if int(passengers) >= passengerCount:
-                ships.append(ship['name'])
-        url = info['next']
+        data = requests.get(url).json()
+
+        for ship in data['results']:
+            try:
+                if int(ship['passengers']) >= passengerCount:
+                    ships.append(ship['name'])
+            except ValueError:
+                if ship['name'] == 'Executor' and ((passengerCount == 99999) or (passengerCount < 99999)):
+                    ships.append('Executor')
+
+        url = data['next']
+
     return ships
